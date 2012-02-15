0265cbbb608f546058f63689cacdd3563c9a65d8
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-02-16 00:12:16 +0800

    scroll problem settled

--- work_sample/scripts/sample.js
+++ work_sample/scripts/sample.js
@@ -1,16 +1,54 @@
 $(function() {
+    $('#info_view').scroll();
+
+    $('#gallery_container').gallery({
+        'width': 700,
+        'height': 450,
+    });
+
     $('#bg_img img').mousedown(function() { // prevent drag
         return false;
     });
 
     $('#bg_img img').css('width', $('html').width());
 
-    $('#info_view').scroll();
+    var page_height = $(window).height();
+    var page_width = $(window).width();
 
-    var page_height = $('body').height();
-    var page_width = $('body').width();
-    $('#gallery_container').gallery({
-        'width': 700,
-        'height': 450,
+    var margin_top = (page_height - $('#container').height()) / 2 - 10;
+    if (margin_top < 0) {
+        margin_top = 0;
+    }
+
+    var margin_left = (page_width - $('#container').width()) / 2;
+    if (margin_left < 0) {
+        margin_left = 0;
+    }
+
+    $('#container').css('margin-top', margin_top);
+    $('#container').css('margin-left', margin_left);
+
+    $('#bg_img img').css('min-height', $('#container').height() + margin_top);
+    $('#bg_img img').css('min-width', $('#container').width() + margin_left);
+
+    $(window).resize(function() {
+        var page_height = $(window).height();
+        var page_width = $(window).width();
+
+        var margin_top = (page_height - $('#container').height()) / 2 - 10;
+        if (margin_top < 0) {
+            margin_top = 0;
+        }
+
+        var margin_left = (page_width - $('#container').width()) / 2;
+        if (margin_left < 0) {
+            margin_left = 0;
+        }
+
+        $('#container').css('margin-top', margin_top);
+        $('#container').css('margin-left', margin_left);
+
+        $('#bg_img img').css('height', page_height);
+        $('#bg_img img').css('width', page_width);
     });
 });
