ec4a536bb8bfc7d387b8a74666f8393c106d25db
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-02-23 21:19:40 +0800

    background problem solved

--- work_sample/scripts/sample.js
+++ work_sample/scripts/sample.js
@@ -1,23 +1,41 @@
-$(function() {
-    $('#work').css('margin-left', '0px');
-    $('#employment').css('margin-right', '0px');
+var img_orig_width, img_orig_height;
+var page_width, page_height;
 
-    $('#info_view').scroll();
+function resize_bg_img() {
+    var bg_img = $('#bg_img img');
+    
+    page_height = $(window).height();
+    page_width = $(window).width();
 
-    $('#gallery_container').gallery({
-        'width': 700,
-        'height': 540,
-    });
+    var img_w_h_ratio = img_orig_width / img_orig_height;
+    var page_w_h_ratio = page_width / page_height;
 
-    $('#bg_img img').mousedown(function() { // prevent drag
-        return false;
-    });
+    if (img_w_h_ratio > page_w_h_ratio) { // image wider
+        var img_new_height = page_height;
+        var img_new_width = Math.ceil(img_orig_width / img_orig_height * img_new_height);
+        var img_margin_left = - (page_width - img_new_width) / 2;
+        if (img_margin_left > 0) {
+            img_margin_left = - img_margin_left;
+        }
+        bg_img.css('height', img_new_height);
+        bg_img.css('width', img_new_width);
+        bg_img.css('margin-left', img_margin_left);
+        bg_img.css('margin-top', '0');
+    } else { // image taller
+        var img_new_width = page_width;
+        var img_new_height = Math.ceil(img_orig_height / img_orig_width * img_new_width);
+        var img_margin_top = - (page_height - img_new_height) / 2;
+        if (img_margin_top > 0) { 
+            img_margin_top = - img_margin_top;
+        }
+        bg_img.css('height', img_new_height);
+        bg_img.css('width', img_new_width);
+        bg_img.css('margin-top', img_margin_top);
+        bg_img.css('margin-left', '0');
+    }
+}
 
-    $('#bg_img img').css('width', $('html').width());
-
-    var page_height = $(window).height();
-    var page_width = $(window).width();
-
+function resize_container() {
     var margin_top = (page_height - $('#container').height()) / 2 - 25;
     if (margin_top < 0) {
         margin_top = 0;
@@ -30,28 +48,36 @@ $(function() {
 
     $('#container').css('margin-top', margin_top);
     $('#container').css('margin-left', margin_left);
+}
 
-    $('#bg_img img').css('min-height', $('#container').height() + margin_top);
-    $('#bg_img img').css('min-width', $('#container').width() + margin_left);
+$(function() {
+    page_height = $(window).height();
+    page_width = $(window).width();
 
+    $('#bg_img img').mousedown(function() { // prevent drag
+        return false;
+    });
+
+    $('#work').css('margin-left', '0px');
+    $('#employment').css('margin-right', '0px');
+
+    $('#info_view').scroll();
+
+    $('#gallery_container').gallery({
+        'width': 700,
+        'height': 545,
+    });
+
+    $('#bg_img img').load(function() {
+        img_orig_width = $('#bg_img img').width();
+        img_orig_height = $('#bg_img img').height();
+        resize_bg_img();
+    });
+
+    resize_container();
+    
     $(window).resize(function() {
-        var page_height = $(window).height();
-        var page_width = $(window).width();
-
-        var margin_top = (page_height - $('#container').height()) / 2 - 25;
-        if (margin_top < 0) {
-            margin_top = 0;
-        }
-
-        var margin_left = (page_width - $('#container').width()) / 2;
-        if (margin_left < 0) {
-            margin_left = 0;
-        }
-
-        $('#container').css('margin-top', margin_top);
-        $('#container').css('margin-left', margin_left);
-
-        $('#bg_img img').css('height', page_height);
-        $('#bg_img img').css('width', page_width);
+        resize_bg_img();
+        resize_container();
     });
 });
