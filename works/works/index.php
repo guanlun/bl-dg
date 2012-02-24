6eb03d450206cbde533931fd01f31f86bd586536
Guanlun ZHAO <guanlunzhao@gmail.com>
2012-02-25 00:04:40 +0800

    warhol added

---
+++ works/works/index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html;charset=UTF-8"/>
+        <title>BL-DG Works</title>
+        <link href="css/sample.css" rel="stylesheet"/>
+        <script src="scripts/jquery-1.7.1.min.js"></script>
+        <script src="scripts/scroll.js"></script>
+        <script src="scripts/gallery.js"></script>
+        <script src="scripts/sample.js"></script>
+    </head>
+    <body>
+        <div id='bg_img'>
+            <img src='images/05_photo.jpg' />
+        </div>
+        <div id='container'>
+            <div id="nav_bar">
+                <ul>
+                    <li id="work">work</li>
+                    <li id="research">research</li>
+                    <li id="words">words</li>
+                    <li id="about">about</li>
+                    <li id="contact">contact</li>
+                    <li id="employment">employment</li>
+                </ul>
+            </div>
+            <div id='inner_container'>
+                <div id='left_container'>
+                    <ul>
+                        <li>featured</li>
+                        <li>status</li>
+                        <li>category</li>
+                        <li>chronology</li>
+                        <li>location</li>
+                    </ul>
+                </div>
+                <div id='middle_container'>
+                    <ul>
+                        <li>the basement</li>
+                        <li>weekend house renovation</li>
+                        <li>new orlean public library</li>
+                        <li>warhol office building</li>
+                        <li>stern facade redesign</li>
+                        <li>daunus</li>
+                        <li>straw-blurry fields</li>
+                        <li>minimal mirror</li>
+                        <li>kartria memoriial</li>
+                        <li>desert retreat</li>
+                        <li>research station</li>
+                        <li>xujiahui highrise (EXH)</li>
+                        <li>flockr-pavilion (SO-IL)</li>
+                        <li>kukje art center (SO-IL)</li>
+                    </ul>
+                </div>
+                <div id='right_container'>
+                    <ul>
+                        <li>residential</li>
+                        <li>education</li>
+                        <li>public space</li>
+                        <li>installation</li>
+                        <li>commercial</li>
+                        <li>cultural</li>
+                        <li>pre BL-DG</li>
+                    </ul>
+                </div>
+            </div>
+            </div>
+        </div>
+    </body>
+</html>
